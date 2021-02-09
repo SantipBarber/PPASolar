97304ae7f1ccaab998ce2f34c98b829c890ea5dd
Santiago Pérez Barber <santipbr@gmail.com>
2021-02-09 17:12:21 +0100

    Inicio del desarrollo de la app PPASolar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./sections/main/main.module').then(m => m.MainModule) },
+  { path: 'main', loadChildren: () => import('./sections/main/main.module').then(m => m.MainModule) },
+  { path: 'about', loadChildren: () => import('./sections/about/about.module').then(m => m.AboutModule) },
+  { path: 'simulator', loadChildren: () => import('./sections/simulator/simulator.module').then(m => m.SimulatorModule) },
+  { path: 'contact', loadChildren: () => import('./sections/contact/contact.module').then(m => m.ContactModule) }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderModule } from './shared/components/header/header.module';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { FooterModule } from './shared/components/footer/footer.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    FooterModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
